f1a7292016b244634645b7dfefc9572a3d16d5bb
iAbdulKader <abdulkadernow@gmail.com>
2022-02-04 15:42:18 +0600

    Changed Upload Box's UI

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -14,15 +14,16 @@ export default function Upload() {
     <div className={styles.main}>
       <div className={`${styles.uploadWrapper} horizontal_center`}>
         <div className={styles.uploadDotted}>
-          <div onDrag={triggerFile} className={`${styles.container} center`}>
+          <div onDrag={triggerFile} className={styles.container}>
              <div className={styles.icons}>
                <FileIcon className={styles.right} />
                <FileIcon className={styles.left} />
                <FileIcon className={styles.center} />
              </div>
              <input ref={fileRef} type="file" />
-             <div>
+             <div className={styles.textContainer}>
               <h4>Drag & Drop Your Files Or <span style={{color: "#0288d1"}} onClick={triggerFile}>Browse</span></h4>
+              <p>Max file size 30MB<span className="highlight">.</span></p>
              </div>
           </div>
         </div>
