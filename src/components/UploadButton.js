a0fba8a32ba5e41d2ca6f77342601a115ad7de29
iAbdulKader <abdulkadernow@gmail.com>
2022-02-04 15:20:39 +0600

    new public upload btn ui

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -1,19 +1,8 @@
 export default function UploadButton() {
-  const divStyle = {
-    fontSize: "13px",
-    color: "#fff",
-    background: "var(--color-button-border)",
-    padding: "4px 15px",
-    borderRadius: "5px",
-    textAlign: "center",
-    curosr: "pointer",
-    userSelect: "none"
-  }
   
   return (
     <div 
       className="center nonAuthButton"
-      style={divStyle}
     >
       Upload Files
     </div>
