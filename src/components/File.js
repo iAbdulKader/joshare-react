72f469b379ad570020ff7383be8156a0137cf769
iAbdulKader <abdulkadernow@gmail.com>
2022-02-11 18:07:12 +0600

    Added icons before files display name

--- src/components/File.js
+++ src/components/File.js
@@ -1,13 +1,23 @@
 import styles from "../styles/File.module.css";
+import { FileIcon, defaultStyles } from 'react-file-icon';
 import { HiOutlineDownload } from "react-icons/hi";
 import { AiOutlineDelete } from "react-icons/ai";
 
 export default function File({auth=true}) {
+  let icon = "jpg";
+  
   return(
     <div className={styles.container}>
-      <div className={styles.textContainer}>
-        <input type="text" value="Some File.jpg" readOnly />
-        <p>3.1MB</p>
+      
+      <div className={styles.fileInfoContainer}>
+        <div className={`${styles.fileIcon} center`}>
+          <FileIcon extension="jpg" {...defaultStyles[icon]} />
+        </div>
+        
+        <div className={styles.textContainer}>
+          <input type="text" value="Some File.jpg" readOnly />
+          <p>3.1MB</p>
+        </div>
       </div>
       
       <div className={styles.btnContainer}>
