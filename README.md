ab703e3f5144bab41b3b7455f70f972ccd8f7b7f
iAbdulKader <abdulkadernow@gmail.com>
2022-02-27 02:44:58 +0600

    New Favicon

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# This is joShare's frontend made with Nextjs, Firebase.
-## Context API ✔️
-## Custom Hooks ✔️
-## Good Practices ✔️
-## Modular Code ✔️
+## This is joShare's frontend made with Nextjs, Firebase.
+### Context API ✔️
+### Custom Hooks ✔️
+### Good Practices ✔️
+### Modular Code ✔️
--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,7 @@ export default function Meta({ title }) {
       <Head>
         <title>{title}</title>
         <meta name="description" content="joShare - A File Sharing App. Share Files With Ease." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/android-chrome-512x512.png" />
       </Head>
     )
 }
