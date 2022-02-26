a16f2240ef3e19bbca8106ad3aa75f581d01811c
iAbdulKader <abdulkadernow@gmail.com>
2022-02-26 13:09:10 +0600

    Added nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 import '../styles/global.css';
+import NProgress from "nprogress";
+import Router from "next/router";
 import ThemeButton from "../components/ThemeButton";
 import { Toaster } from 'react-hot-toast';
 import GlobalProvider from "../contexts/GlobalContext";
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({
+    showSpinner: true
+  })
+  
+  Router.events.on('routeChangeStart', ()=>{
+    NProgress.start();
+  });
+  Router.events.on('routeChangeComplete', ()=>{
+    NProgress.done();
+  });
+  Router.events.on('routeChangeError', ()=>{
+    console.log("Error")
+  });
+  
   return (
     <GlobalProvider>
       <Component {...pageProps} />
