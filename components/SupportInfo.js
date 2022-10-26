08a3557d50d169e5357c002cf7c59c3a6c49b70d
Abdul Kader <88508957+iAbdulKader@users.noreply.github.com>
2022-10-27 00:27:57 +0600

    Update SupportInfo.js

--- components/SupportInfo.js
+++ components/SupportInfo.js
@@ -7,7 +7,7 @@ export default function SupportInfo() {
     <div className={`${styles.container} ${styles.supportContainer}`}>
       <div className={styles.infoWrapper}>
         <h2>Contact Details</h2>
-        <h4><HiOutlineMail style={{verticalAlign: "middle"}} /> : <a href="mailto:support@joshare.ml">support@joshare.ml</a></h4>
+        <h4><HiOutlineMail style={{verticalAlign: "middle"}} /> : <a href="mailto:support@joshare.ml">support@joshare.xyz</a></h4>
         <h4><AiOutlineTwitter style={{verticalAlign: "middle"}} /> : <a href="https://twitter.com/itsabdulkader" target="_blank" rel="noreferrer">@itsabdulkader</a></h4>
       </div>
     </div>
