28cdbea031e0c66608c00ef8f9b8e4052dd71165
iabdulkader <abdulkadernow@gmail.com>
2022-11-17 01:50:18 +0600

    Fix Theme Switcher

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,8 +1,35 @@
 import styles from "../styles/ThemeToggle.module.scss";
-import useTheme from "../hooks/useTheme";
+import { useState, useEffect } from "react";
 
 export default function Theme() {
-  const { activeTheme, toggleTheme } = useTheme();
+  const [activeTheme, setActiveTheme] = useState("");
+
+    const toggle = () => {
+        console.log("clicked");
+        setActiveTheme(activeTheme === "dark" ? "light" : "dark")
+    }
+
   
-  return <input onChange={toggleTheme} id="toggle" className={styles.toggle} type="checkbox" checked={activeTheme === "dark" ? true : false}  />
+      useEffect(() => {
+
+        if(window.localStorage.getItem("theme")) {
+          setActiveTheme(window.localStorage.getItem("theme"))
+        }
+
+        if(activeTheme === "light") {
+            document.body.dataset.theme = activeTheme;
+            localStorage.theme = "light";
+            setActiveTheme("light")
+        } 
+        
+        if(activeTheme === "dark") {
+            setActiveTheme("dark")
+            localStorage.theme = "dark";
+            document.body.dataset.theme = activeTheme;
+        }
+
+      }, [activeTheme]);
+
+  
+  return <input onChange={toggle} id="toggle" className={styles.toggle} type="checkbox" checked={activeTheme === "dark" ? true : false}  />
 }
